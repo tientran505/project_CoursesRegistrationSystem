ad8560b7724d8662260672b01ddc14aa19d95b3b
AnnThư <80025852+anhthu872@users.noreply.github.com>
2021-04-02 00:31:55 +0700

    student.h bổ sung

--- Courses Registration System/Courses Registration System/student.h
+++ Courses Registration System/Courses Registration System/student.h
@@ -8,13 +8,25 @@ struct Class {
 	wstring Name_of_Subject; //ten mon hoc
 	string ID_of_subject; //ma mon hoc
 	int credit; // so tin chi
+	string schedule_theory;
+	string schedule_lab;
 	string Name_of_Teacher;
+};
+struct student_Scoreboard {
+	double total_Mark;
+	double final_Mark;
+	double midterm_Mark;
+	double gpa;
+	string grade; // C->A+
 };
 struct Subjects {
 	Class Subject; 
 	bool registers; // 0: khong dang ki mon; 1: dang ki mon
-	bool stranfer; // 0 khong doi lop; 1: co doi lop
-	double score; // khong dang ki thi diem mac dinh -1; da dang ki thi co diem
+	bool stranfer; // 0 khong doi lop; 1: co doi lop so voi lop duoc sap vao
+	bool retake; // 0: khong hoc lai; 1: hoc lai
+	student_Scoreboard score;
+	int semester;
+	int year;
 	Subjects* pNext, * pPrevious;
 };
 
@@ -32,8 +44,11 @@ struct NodeStudent {
 	string Gender; //gioi tinh
 	unsigned int Social_ID;
 	Date Date_Of_Birth;
+	string email_Student;
+	string email;
+	string phone_number;
 	double gpa; //diem tong
-	Subjects* y; //mon va diem theo mon
+	Subjects y; //mon va diem theo mon
 };
 
 struct _NodeStudent {
