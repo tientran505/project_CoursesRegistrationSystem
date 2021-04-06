0acc2981a3e33f32f3789ec8cb50a5b6a1ce0425
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2021-04-06 08:02:58 +0700

    phanhuyCommit

--- Courses Registration System/Courses Registration System/main.cpp
+++ Courses Registration System/Courses Registration System/main.cpp
@@ -2,8 +2,14 @@
 #include "student.h"
 
 int main() {
-	const char* path = "D:\\Desktop\\testdemo.csv";
+	const char* path = "D:\\Desktop\\hello.csv";
 	_NodeStudent* student = nullptr;
 	currentDateTime();
 	ReadStudent_CSV(path, student);
+	_NodeStudent* pCur = student;
+	while (pCur != nullptr)
+	{
+		cout << pCur->data.Firstname;
+		pCur = pCur->pNext;
+	}
 }
