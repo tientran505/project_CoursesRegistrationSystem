efa716e834673775ec8861fcc834779893cfabd3
Tien <73010955+tientran505@users.noreply.github.com>
2021-04-11 14:07:20 +0700

    create login system + menu for stu + staff

--- Courses Registration System/Courses Registration System/main.cpp
+++ Courses Registration System/Courses Registration System/main.cpp
@@ -4,11 +4,6 @@
 
 int main() {
 	currentDateTime();
-	//_Student* head = nullptr;
-	//string path;
-	//listStudents(path, head);
-	//logInSystem_Student(head);
-	//deleteStudentList(head);
 	log_In_System();
 	return 0;
 }
