0360799b0eba6fbbb94b485bce26beb9b316fabb
AnnThư <80025852+anhthu872@users.noreply.github.com>
2021-04-12 10:22:35 +0700

    coursesresult

--- Courses Registration System/Courses Registration System/student.h
+++ Courses Registration System/Courses Registration System/student.h
@@ -2,7 +2,8 @@
 
 void showInfo_Student(_Student* Node);
 void editPassword(_Student* Node);
-void readCourse(_Student* Node);
+void CoursesRegistration(_Student* Node);
+void CourseRegistrationResult(_Student* Node);
 void student_Menu(_Student* Node);
 _Student* logInSystem_Student(_Student* head);
 
